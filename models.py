d983e78e867510c35485187af175df511ca519fe
psitoleux <paul.sitoleux+github@me.com>
2024-02-10 18:49:24 +0100

    indent issue"

--- models.py
+++ models.py
@@ -5,18 +5,18 @@ from torch import nn
 class MLPModel(nn.Module):
     def __init__(nin, nlayers, nhid, nout)
 
-    super(MLPModel, self).__init__()
+        super(MLPModel, self).__init__()
 
-    self.input_layer = nn.Linear(nin, nhid)
+        self.input_layer = nn.Linear(nin, nhid)
 
-    self.hidden_layers = []
+        self.hidden_layers = []
 
-    for i in range(nlayers):
-        self.hidden_layers += [nn.Linear(nhid, nhid)]
+        for i in range(nlayers):
+            self.hidden_layers += [nn.Linear(nhid, nhid)]
 
-    self.hidden_layers = nn.ModuleList(self.hidden_layers)
+        self.hidden_layers = nn.ModuleList(self.hidden_layers)
 
-    self.output_layer = nn.Linear(nhid, nout)
+        self.output_layer = nn.Linear(nhid, nout)
 
     def forward(x):
 
